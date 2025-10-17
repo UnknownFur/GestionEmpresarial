15a0e72495f2bb32b51e7ac605b9521a770ffdb8
UnknownFur <123666616+UnknownFur@users.noreply.github.com>
2025-10-17 14:36:03 -0500

    AAAAAAAAaaa

--- RANextCity/Assets/Scripts/MapScript/ARSessionManager.cs
+++ RANextCity/Assets/Scripts/MapScript/ARSessionManager.cs
@@ -6,39 +6,16 @@ using TMPro;
 public class ARSessionManager : MonoBehaviour
 {
     [SerializeField] private Button backButton;
-    public TextMeshProUGUI descriptionText; // ← referencia al texto del Canvas
-
 
     void Start()
     {
-        int currentPOIIndex = RouteManager.Instance.currentPOIIndex;
-        Debug.Log($"📍 AR iniciado para POI #{currentPOIIndex}");
-
         if (backButton != null)
             backButton.onClick.AddListener(OnBackToMap);
         backButton.onClick.AddListener(OnBackToMap);
-
-        // Buscar el POI actual y mostrar su descripción
-        int index = RouteManager.Instance.currentPOIIndex;
-
-        POI[] pois = FindObjectsOfType<POI>();
-        if (index < pois.Length && descriptionText != null)
-        {
-            descriptionText.text = pois[index].description;
-            Debug.Log($"Mostrando descripción del POI: {pois[index].poiName}");
-        }
-        else
-        {
-            Debug.LogWarning("No se pudo mostrar la descripción del POI (índice fuera de rango o sin referencia).");
-        }
-
-        
     }
 
     void OnBackToMap()
     {
-        Debug.Log("🔙 Saliendo del modo AR... avanzando al siguiente POI");
-
         // Avanzar al siguiente POI
         RouteManager.Instance.NextPOI();
 
---
+++ RANextCity/Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVEwS3RSSC8vLy8vQUFBQm1aWWFGWXdHNEVzSG9wZE5ITHdJWHVscUVTV1FlSXV3YTMvWlhXWHczd3VRRzZFWHpJOHUrUm5aakN4Yk1xYjJUTDhiOG5SWlRLaXZQZU1QenJBQk9qSFU0M3pBdm9lVGxSajV2T24vMjJ4SlBSaXl3UmEwMitCTDc2Ly9YaFhIVS9jbjlaNUlwSFFGS2EwZ1FZL01CcWQ4bXRUMGJaM2JiWURGR09CNThtRmlORjRlN0NCRFFYV1JCb1FUMmJnYjY2cDRTR3MvbzlwUE1yVlRKeDN6c3QyT21BS3RzOU5MMHRVdkNRY1Q1aGhtSjYvTDZwZkRTV3lGUVZjcnlqS09HREphRmhyL1YyK3oxUEV6YlByWjFnN0xuM09XdGVLeENMQUZFZGdDRjFBWFJRajc3T0I2VUVtU0FudndxY1NhWURQN0tvT2ZIbDI2aGhlK0JwcmM2c3RYV1VkYzlEUDduVWlKWG9GNC9wVE0=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
